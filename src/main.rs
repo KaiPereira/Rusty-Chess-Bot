a84349fd2217831610083cc2c38a60c5ba16ea4a
Kai Pereira <88850028+KaiPereira@users.noreply.github.com>
2024-01-09 20:36:30 +0000

    Parse FEN implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    parse_fen()
+}
+
+
+pub fn parse_fen() {
+    let fen: &str = "rnbqkbnr/ppp1pppp/8/3p4/2PP4/8/PP2PPPP/RNBQKBNR b KQkq c3 0 2";
+    let fen_values: Vec<_> = fen.split_whitespace().collect();
+    let board_fen: &str = fen_values[0];
+    
+    // Our board
+    let mut board: Vec<char> = Vec::new();
+
+    for section in board_fen.split("/").collect::<Vec<&str>>() {
+
+        for char in section.chars() {    
+            if char.is_numeric() {
+                let amount_of_zeros = char.to_digit(10).unwrap();
+
+                // Push that many zeros to an array or push the letter
+                for _ in 0..amount_of_zeros {
+                    board.push('0');
+                }
+
+            } else {
+                board.push(char);
+            }
+        }
+    }
+
+    println!("{:?}", board)
 }
---
+++ src/readme.md
@@ -0,0 +1,7 @@
+# Steps
+
+Chess engine completely from scratch
+
+1. FEN to 64 int bitboard
+- First create a method to create an empty 64 bit bitboard filled with 0s
+- Next set the starting FEN
